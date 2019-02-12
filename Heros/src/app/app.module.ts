588fed7bcf3c43fbb78b4300aacfd659d28ce326
unknown <obieda01@email.franklin.edu>
2019-02-12 01:25:18 -0500

    Fep 12 practice

--- Heros/src/app/app.module.ts
+++ Heros/src/app/app.module.ts
@@ -2,19 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent,
-    FormsModule
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
